d978a05dc4a614f92a811c698bec430e8318af31
Jason Huynh <tech.junkie.jh@gmail.com>
2019-08-18 23:25:15 -0400

    Hour 4 changes

--- NorthwindCafe.Web/Startup.cs
+++ NorthwindCafe.Web/Startup.cs
@@ -15,6 +15,7 @@ namespace NorthwindCafe.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,7 +27,13 @@ namespace NorthwindCafe.Web
             }
             app.UseDefaultFiles();
             app.UseStaticFiles();
-            app.UseMvc();
+            app.UseMvc(config => {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" }
+                    );
+            });
         }
     }
 }
